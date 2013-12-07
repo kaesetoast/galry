6e9a83ec4a7c188f4b367eddbd1d640237d8df85
Philipp Nowinski <philipp@nowinski.de>
2013-12-07 19:21:18 +0100

    more boilerplate

---
+++ src/galry.js
@@ -0,0 +1,17 @@
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) define(factory);
+    else if (typeof exports === 'object') module.exports = factory();
+    else root.galry = factory();
+}(this, function() {
+
+    'use strict';
+
+    var playerElement;
+
+    function galry() {
+        return galry;
+    }
+
+    return galry;
+
+}));
