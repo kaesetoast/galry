6d8fe7b7156d700883eaf7edfaf2e5f91322213b
Philipp Nowinski <philipp@nowinski.de>
2014-02-06 12:16:37 +0100

    forgotten semicolon

--- dist/galry.js
+++ dist/galry.js
@@ -1,7 +1,7 @@
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) define(factory);
     else if (typeof exports === 'object') module.exports = factory();
-    else root.galry = factory()
+    else root.galry = factory();
 }(this, function() {
 
 'use strict';
