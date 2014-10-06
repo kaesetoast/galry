27bce4201f0d43ae54a6b738b9f771e1851f1d99
Philipp Nowinski <philipp@nowinski.de>
2014-10-06 11:41:20 +0200

    added server

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,15 @@ module.exports = function(grunt) {
             options: {
                 livereload: 35729
             }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: '.'
+                }
+            }
         }
 
     });
@@ -69,6 +78,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-jsbeautifier');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'jsbeautifier', 'uglify', 'watch']);
+    grunt.registerTask('default', ['jshint', 'concat', 'jsbeautifier', 'uglify', 'connect', 'watch']);
 
 };
